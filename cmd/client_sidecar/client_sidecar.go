b63465ce4cf14e3e5da5ec3a889513a3cb551553
sanabriad <sanabria.d@gmail.com>
2020-08-11 11:05:09 +0100

    Testing file sharing

--- cmd/client_sidecar/client_sidecar.go
+++ cmd/client_sidecar/client_sidecar.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strconv"
 	"time"
 )
+
+const imds = "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01"
 
 type responseJson struct {
 	AccessToken string `json:"access_token"`
@@ -19,12 +23,12 @@ type responseJson struct {
 func getAccessToken(resource string) responseJson {
 	// Create HTTP request for a managed services for Azure resources token to access Azure Resource Manager
 	var msi_endpoint *url.URL
-	msi_endpoint, err := url.Parse("http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01")
+	msi_endpoint, err := url.Parse(imds)
 	if err != nil {
 		log.Panicln(err)
 	}
 	msi_parameters := url.Values{}
-	msi_parameters.Add("resource", "https://management.azure.com/")
+	msi_parameters.Add("resource", resource)
 	msi_endpoint.RawQuery = msi_parameters.Encode()
 	req, err := http.NewRequest("GET", msi_endpoint.String(), nil)
 	if err != nil {
@@ -55,12 +59,28 @@ func getAccessToken(resource string) responseJson {
 	return r
 }
 
+func shareToken(token string, path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	w := bufio.NewWriter(file)
+	bytes, err := w.WriteString(token)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Wrote %d bytes to %s", bytes, path)
+	return err
+}
+
 func main() {
 	resp := getAccessToken("https://management.azure.com/")
+	shareToken(resp.AccessToken, "/token/.token")
 	duration, err := strconv.Atoi(resp.ExpiresIn)
 	if err != nil {
-		log.Panic(err)
+		log.Panicln(err)
 	}
-	fmt.Printf("Sleeping for %s seconds ...", resp.ExpiresIn)
+	fmt.Printf("Sleeping for %s seconds ...\n", resp.ExpiresIn)
 	time.Sleep(time.Duration(duration) * time.Second)
 }
